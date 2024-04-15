4f96088ef70beb3cf924a117ec9ec6882888c011
Ivo Kostov <kostov.ivo@gmail.com>
2024-04-15 22:20:33 +0300

    add cors headers, also check index.js:35                                  if (error) return callback(error);

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const { addUser, removeUser, getUser, getUsersInRoom } =
 // const cors = require('cors');
 const PORT = 5000; //  process.env.PORT || 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
