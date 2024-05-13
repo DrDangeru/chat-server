85b0ad52fded0062d50ed597581b0fa0386c1fc3
Nikola Kostov <kostov.koko@gmail.com>
2024-05-13 22:28:02 +0300

    errors in send/res after file functionality add

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const socketio = require('socket.io')
-const { createServer } = require('node:http');
+const { createServer, request } = require('node:http');
 const router = require('./router.js');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 // const { clearScreenDown } = require('node:readline');
 const PORT = 5000;
 const app = express();
+const { writeFile } = require('fs');
 
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
+  maxHttpBufferSize: 1e8,
   cors: {
     origin: "http://localhost:3000",
   }
 });
-
 
 io.on('connection', (socket) => {
   // console.log('A user connected');
@@ -30,24 +31,50 @@ io.on('connection', (socket) => {
     callback(error => console.log(error));
   });
 
-  socket.on('sendMessage', ({ message, name, room }) => {
-    io.to(room).emit('message', {
+  socket.on('sendMessage', ({ message, name, room, type, body, mimeType, fileName }) => {
+    const messageObj = type === 'file' ? {
+      user: name,
+      text: message,
+      room: room,
+      type: type,
+      body: body,
+      mimeType: mimeType,
+      fileName: fileName
+    } : {
       user: name,
       text: message,
       room: room
-    });
+    };
+
+    io.to(room).emit('sendMessage', messageObj);
+    console.log('Message sent:', messageObj);
   });
 
-  socket.on('message', ({ message, name }) => {
-    io.to(room).emit('message',
-      {
-        user: name, // message.user
-        text: message.text, // `${message}`
-        room: room
-      });
+  // socket.on('sendMessage', ({ message, name, room }) => {
+  //   io.to(room).emit('message', {
+  //     user: name,
+  //     text: message,
+  //     room: room
+  //   });
+  // });
 
+  socket.on('message', ({ message, name, room, type, body, mimeType, fileName }) => {
+    const messageObj = type === 'file' ? {
+      user: name,
+      text: message,
+      room: room,
+      type: type,
+      body: body,
+      mimeType: mimeType,
+      fileName: fileName
+    } : {
+      user: name,
+      text: message,
+      room: room
+    };
+
+    console.log('Message received:', messageObj);
   });
-
 
   socket.on('disconnect', (reason) => {
     console.log('A user disconnected');
@@ -60,6 +87,12 @@ io.on('connection', (socket) => {
     }
   });
 
+  // socket.on("upload", (file, callback) => {
+  //   console.log(file); // <Buffer 25 50 44 ...>
+  //   writeFile("/tmp/upload", file, (err) => {
+  //     callback({ message: err ? "failure" : "success" });
+  //   });
+  // });
 
 });
 httpServer.listen(PORT, () => console.log(`Socket.IO server listening on port ${PORT}`))
